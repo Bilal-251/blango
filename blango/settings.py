8986bd299bb337e0ce5d8794bdf1449052f26de8
Muhammad Yousaf <codeninja626@gmail.com>
2023-11-20 15:36:08 +0000

    Finish caching

--- blango/settings.py
+++ blango/settings.py
@@ -155,7 +155,7 @@ LOGGING = {
         "level": "DEBUG",
     },
 }
-  PASSWORD_HASHERS = [
+PASSWORD_HASHERS = [
       'django.contrib.auth.hashers.Argon2PasswordHasher',
       'django.contrib.auth.hashers.PBKDF2PasswordHasher',
       'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -4,6 +4,8 @@ from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
 from blog.models import Post
+import logging
+logger = logging.getLogger(__name__)
 user_model = get_user_model()
 
 register = template.Library()
@@ -46,4 +48,5 @@ def row(extra_classes=""):
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
     posts = Post.objects.exclude(pk=post.pk)[:5]
+    logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
     return {"title": "Recent Posts", "posts": posts}
