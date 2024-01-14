e4f8773b9a1ace973ff9f42ee24c3ab7b9bfb8aa
Muhammad Yousaf <codeninja626@gmail.com>
2024-01-14 16:14:21 +0000

    Finish Serializers

--- blog/api_views.py
+++ blog/api_views.py
@@ -1,5 +1,7 @@
+from blog.api.serializers import PostSerializer
 import json
 from http import HTTPStatus
+
 
 from django.http import JsonResponse, HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import get_object_or_404
@@ -9,26 +11,14 @@ from django.views.decorators.csrf import csrf_exempt
 from blog.models import Post
 
 
-def post_to_dict(post):
-    return {
-        "pk": post.pk,
-        "author_id": post.author_id,
-        "created_at": post.created_at,
-        "modified_at": post.modified_at,
-        "published_at": post.published_at,
-        "title": post.title,
-        "slug": post.slug,
-        "summary": post.summary,
-        "content": post.content,
-    }
+
 
 
 @csrf_exempt
 def post_list(request):
     if request.method == "GET":
         posts = Post.objects.all()
-        posts_as_dict = [post_to_dict(p) for p in posts]
-        return JsonResponse({"data": posts_as_dict})
+        return JsonResponse({"data": PostSerializer(posts, many=True).data})
     elif request.method == "POST":
         post_data = json.loads(request.body)
         post = Post.objects.create(**post_data)
@@ -45,7 +35,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
     if request.method == "GET":
-        return JsonResponse(post_to_dict(post))
+        return JsonResponse(PostSerializer(post).data)
     elif request.method == "PUT":
         post_data = json.loads(request.body)
         for field, value in post_data.items():
