1f5530881ec49e2349a365aa667d24a42b942cf7
Muhammad Yousaf <codeninja626@gmail.com>
2024-02-04 15:51:17 +0000

    Finish permissions

--- blango/settings.py
+++ blango/settings.py
@@ -117,7 +117,10 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
-    ]
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+    ],
 }
 
 # Internationalization
