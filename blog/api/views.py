8f8cdfb5e56fcbef6d7bf0974af3b9e1d021edca
Muhammad Yousaf <codeninja626@gmail.com>
2024-02-06 09:38:32 +0000

    Finish nested relationships

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from blog.api.permissions import AuthorModifyOrReadOnly,IsAdminUserForObject
 from django.contrib.auth.models import User
-from blog.api.serializers import PostSerializer, UserSerializer
+from blog.api.serializers import PostSerializer, UserSerializer,PostDetailSerializer
 from blog.models import Post
 
 
